dd4c77e9f418a03bd32b288fd7387181c2c6195e
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-10-18 16:05:41 +0900

    #1 헤더 디자인 변경

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header.js';
-import Landingp from '../components/Landing.js';
+import Home from '../components/Landing.js';
 import Footer from '../components/Footer.js';
 
 
@@ -8,7 +8,7 @@ const Landing = () => {
   return (
     <div>
       <Header/>
-      <Landingp/>
+      <Home/>
       <Footer/>
     </div>
   );
