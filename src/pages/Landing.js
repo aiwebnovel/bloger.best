c02aef91fedbfe6e754e3041602b6359ef90a182
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-10-19 16:37:37 +0900

    #1 landing page 수정

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Header from '../components/Header.js';
 import Home from '../components/Landing.js';
 import Footer from '../components/Footer.js';
-
+import { ResponsiveContext } from "grommet";
 
 const Landing = () => {
+  const size = useContext(ResponsiveContext);
+  
+  const mediumPoint = {
+    medium : {
+      value: 1024,
+        edgeSize: {
+          none: '0px',
+          small: '12px',
+          medium: '24px',
+          large: '48px',
+        },
+    }
+  }
+
   return (
     <div>
       <Header/>
-      <Home/>
+      <Home sizes={size} medium={mediumPoint}/>
       <Footer/>
     </div>
   );
