a8e427c7c7e961fb94d8ed6a705dad42afe1d2b3
ho-joon-kim <hojoon.dev@gmail.com>
2021-10-26 19:36:15 +0900

    update - address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,38 @@
-import { Component } from 'react';
-import '../style/Footer.css';
+import { Component } from "react";
+import "../style/Footer.css";
 
 class Footer extends Component {
-
-
-
   render() {
     return (
-    <footer>
-    <p class="info">
-      <a href="mailto:support@appplatform.co.kr" class='email'>support@appplatform.co.kr </a>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href="https://appplatform.notion.site/e658644587444247a00af2d05dd69b30" class='email'> 개인정보 처리방침</a><br/>
-      <a href="https://appplatform.notion.site/8be8232fff0341799cf8c13728610b6b" class='email'> 이용약관 </a><br/>
-      
-      Address: #702 BS B/D seocho-daero 334 ,Seocho-gu, Seoul, Korea<br/>
-      ㈜Appplatform 115-87-01388 대표 김춘남 115-87-01388  02-6959-4330<br/>
-      ©Appplatform, Inc All Rights Reserved
-    </p>
-
-    </footer>
+      <footer>
+        <p class="info">
+          <a href="mailto:support@appplatform.co.kr" class="email">
+            support@appplatform.co.kr{" "}
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a
+            href="https://appplatform.notion.site/e658644587444247a00af2d05dd69b30"
+            class="email"
+          >
+            {" "}
+            개인정보 처리방침
+          </a>
+          <br />
+          <a
+            href="https://appplatform.notion.site/8be8232fff0341799cf8c13728610b6b"
+            class="email"
+          >
+            {" "}
+            이용약관{" "}
+          </a>
+          <br />
+          Address: #301 BS B/D banpodaero 28gil ,Seocho-gu, Seoul, Korea
+          <br />
+          ㈜Appplatform 115-87-01388 대표 김춘남 115-87-01388 02-6959-4330
+          <br />
+          ©Appplatform, Inc All Rights Reserved
+        </p>
+      </footer>
     );
   }
 }
